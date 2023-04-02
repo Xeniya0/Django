e6e0343a48f9707553bbedabc191b3252324ecb2
Kseniya <kseniya00.00.00@mail.ru>
2023-04-02 22:22:24 +0300

    процесс дальше

--- saitic/news/views.py
+++ saitic/news/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 def news_home(request):
-    news = Article.objects.all()
+    news = Article.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была заполнена неверно'
+
+    form = ArticleForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
