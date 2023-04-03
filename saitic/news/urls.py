8570f3712751f5b62e00080f7e02d38d438b10a1
Kseniya <kseniya00.00.00@mail.ru>
2023-04-03 20:27:10 +0300

    Revert "процесс дальше"
    This reverts commit e6e0343a48f9707553bbedabc191b3252324ecb2.

--- saitic/news/urls.py
+++ saitic/news/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create, name='create'),
-
 ]
--- saitic/news/views.py
+++ saitic/news/views.py
@@ -1,26 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Article
-from .forms import ArticleForm
 
 def news_home(request):
-    news = Article.objects.order_by('-date')
+    news = Article.objects.all()
     return render(request, 'news/news_home.html', {'news': news})
-
-def create(request):
-    error = ''
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-        else:
-            error = 'Форма была заполнена неверно'
-
-    form = ArticleForm()
-
-    data = {
-        'form': form,
-        'error': error
-    }
-
-    return render(request, 'news/create.html', data)
